6fcaa911aef708ad1e9d8b20c5994532a3a72179
Ayush Panchaity <panchaityayush@gmail.com>
2023-07-24 23:06:52 +0530

    solved Investment Problem

--- Investment_Problem/Investment_problem.py
+++ Investment_Problem/Investment_problem.py
@@ -16,7 +16,7 @@ X6= basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X6")
 
 
 # third step set objective function
-basic_model.setObjective(0.865*X1 + 0.0952*X2 + .10*X3 + 0.0875*X4 +0.0925*X5 +0.09*X6, GRB.MAXIMIZE)
+basic_model.setObjective(0.0865*X1 + 0.0952*X2 + .10*X3 + 0.0875*X4 +0.0925*X5 +0.09*X6, GRB.MAXIMIZE)
 
 # fourth step Add constraints 
 """first constraint"""
@@ -34,7 +34,7 @@ basic_model.addConstr(X6 <= 187500,"25percent for decision variable 6")
 basic_model.addConstr(X1 + X2  +X4  +X6  >= 375000,"Long Investment")
 
 """fourth constraint"""
-basic_model.addConstr(X1 +X4  +X6  <= 262500,"High Risk Investment")
+basic_model.addConstr(X2 +X3  +X5  <= 262500,"High Risk Investment")
 
 
 
---
+++ Investment_problem.sol
@@ -0,0 +1,8 @@
+# Solution for model basic_model
+# Objective value = 68902.5
+X1 112500
+X2 75000
+X3 187500
+X4 187500
+X5 0
+X6 187500
