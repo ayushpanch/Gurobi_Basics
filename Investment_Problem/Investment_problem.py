b6361348a27a9e064667e196d5b5ab46635e6757
Ayush Panchaity <panchaityayush@gmail.com>
2023-07-24 22:44:50 +0530

    added investment problem

--- Investment_Problem/Investment_problem.py
+++ Investment_Problem/Investment_problem.py
@@ -6,28 +6,46 @@ basic_model=gp.Model("basic_model")
 print(basic_model)  
 
 # second step creating of variables 
-X = basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X")
-Y= basic_model.addVar(vtype =GRB.CONTINUOUS,name = "Y")
+X1 = basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X1")
+X2= basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X2")
+X3 = basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X3")
+X4= basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X4")
+X5 = basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X5")
+X6= basic_model.addVar(vtype =GRB.CONTINUOUS,name = "X6")
+
 
 
 # third step set objective function
-basic_model.setObjective(50*X + 100*Y,GRB.MINIMIZE)
+basic_model.setObjective(0.865*X1 + 0.0952*X2 + .10*X3 + 0.0875*X4 +0.0925*X5 +0.09*X6, GRB.MAXIMIZE)
 
 # fourth step Add constraints 
-basic_model.addConstr(7*X + 2*Y>=28,"first constraint")
-basic_model.addConstr(2*X + 12*Y>=24,"second constraint")
-basic_model.addConstr(X>=0,"fourth constraint")
-basic_model.addConstr(X>=0,"fifth constraint")
+"""first constraint"""
+basic_model.addConstr(X1 + X2 + X3 +X4 +X5 +X6  == 750000,"total invest amount")
+
+"""second constraint"""
+basic_model.addConstr(X1 <= 187500,"25percent for decision variable 1")
+basic_model.addConstr(X2 <= 187500,"25percent for decision variable 2")
+basic_model.addConstr(X3 <= 187500,"25percent for decision variable 3")
+basic_model.addConstr(X4 <= 187500,"25percent for decision variable 4")
+basic_model.addConstr(X5 <= 187500,"25percent for decision variable 5")
+basic_model.addConstr(X6 <= 187500,"25percent for decision variable 6")
+
+"""third constraint"""
+basic_model.addConstr(X1 + X2  +X4  +X6  >= 375000,"Long Investment")
+
+"""fourth constraint"""
+basic_model.addConstr(X1 +X4  +X6  <= 262500,"High Risk Investment")
+
 
 
 # write the linear programming equation
-basic_model.write('Week_3_quiz.lp')
+basic_model.write('Investment_problem.lp')
 
 # optimise the solution
 basic_model.optimize()
 
 # write the solution to .sol file
-basic_model.write("Week_3_quiz.sol")
+basic_model.write("Investment_problem.sol")
 
 
 basic_model
