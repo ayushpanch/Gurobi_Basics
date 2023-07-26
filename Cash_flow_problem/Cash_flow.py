7fef92bc5fe509eb08253cad5250c59197d9457e
Ayush Panchaity <panchaityayush@gmail.com>
2023-07-26 09:59:45 +0530

    added_cash_Flow_problem

--- Cash_flow_problem/Cash_flow.py
+++ Cash_flow_problem/Cash_flow.py
@@ -10,18 +10,14 @@ print(basic_model)
 # second step creating of variables 
 
 
-decision_variables= basic_model.addVars([x for x in range(1,6)],vtype =GRB.CONTINUOUS,name ="Decision Variables")
+decision_variables= basic_model.addVars([x for x in range(9)],vtype =GRB.CONTINUOUS,name ="Decision Variables")
 basic_model.update()
 
 #objective functions
-basic_model.setObjective( (basic_model.getVars()[1] + 1*basic_model.getVars()[1])  \
-                        + (basic_model.getVars()[3] + basic_model.getVars()[3]*1.9) \
-                        + (basic_model.getVars()[4] +basic_model.getVars()[4]*1.5 )   ,\
-                        GRB.MAXIMIZE)
+basic_model.setObjective(basic_model.getVars()[1] + 1.9*basic_model.getVars()[3] +\
+                          1.5*basic_model.getVars()[4] + 1.08*basic_model.getVars()[7]
+                        ,GRB.MAXIMIZE)
 
-#another way to write the objective function is 
-# weights =[.0865,.0952,.10,.0875,.0925,.09]
-# basic_model.setObjective(gp.quicksum((a*b )for a,b in zip(weights,basic_model.getVars())),GRB.MAXIMIZE)
 
 
 
@@ -29,20 +25,15 @@ basic_model.setObjective( (basic_model.getVars()[1] + 1*basic_model.getVars()[1]
 """first constraint"""
 basic_model.addConstr(basic_model.getVars()[0] + \
                       basic_model.getVars()[2] +\
-                      basic_model.getVars()[4]    == 100000,"initial investment")
+                      basic_model.getVars()[4] + basic_model.getVars()[5]   == 100000,"initial investment")
 
 
 """second constraint"""
-
+basic_model.write("cashflow_ip.lp")
 basic_model.addConstrs(x <=75000  for x in basic_model.getVars())
 
 """third constraint"""
-basic_model.addConstr( ((basic_model.getVars()[0] + basic_model.getVars()[0]*.5) 
-                         + (basic_model.getVars()[2] + basic_model.getVars()[2]*1.2))  
-
-                         +.8*((basic_model.getVars()[0] + basic_model.getVars()[0]*.5) 
-                         + (basic_model.getVars()[2] + basic_model.getVars()[2]*1.2))\
-                         
+basic_model.addConstr(0.5 * (basic_model.getVars()[0]) + 1.2*(basic_model.getVars()[2]) + 1.08*basic_model.getVars()[5]   
                             ==  basic_model.getVars()[1],"Long Investment")
 
 """fourth constraint"""
