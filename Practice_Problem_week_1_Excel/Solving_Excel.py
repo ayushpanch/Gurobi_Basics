baa4c71ba1e6a80c1b2ad55ed4a5d3e2e6aa3b03
Ayush Panchaity <panchaityayush@gmail.com>
2023-07-27 15:49:30 +0530

    using Excel we have solved the Problem

--- Practice_Problem_week_1_Excel/Solving_Excel.py
+++ Practice_Problem_week_1_Excel/Solving_Excel.py
@@ -13,7 +13,7 @@ basic_model=gp.Model("basic_model")
 print(basic_model)  
 
 
-Decision_variable_names = ["A", "B", "C", "D","E"]
+Decision_variable_names = df['Bond_Name'].tolist()
 
 # Create decision variables with meaningful names using list comprehension
 decision_vars = [basic_model.addVar(vtype=GRB.CONTINUOUS, name=individual_bond_name) for individual_bond_name in Decision_variable_names]
@@ -22,7 +22,7 @@ decision_vars = [basic_model.addVar(vtype=GRB.CONTINUOUS, name=individual_bond_n
 basic_model.update()
 
 # third step setting up the objective functions 
-tax_yield_return =[0.043,.027,.025,.022,.045]
+tax_yield_return =df['Tax_Yield'].tolist()
 basic_model.setObjective(gp.quicksum((a*b) for a,b in zip(tax_yield_return,basic_model.getVars())),GRB.MAXIMIZE)   
 
 # fourth step to add constraints 
@@ -31,38 +31,33 @@ basic_model.addConstr(gp.quicksum(basic_model.getVars()) <=10 ,"initial investme
 
 
 """second constraint criteria Government and Agency bond must be invested atleast 4 millions"""
-basic_model.addConstr(basic_model.getVars()[1]  +basic_model.getVars()[2] + basic_model.getVars()[3] >= 4, \
+indexes_of_dec_var=df[df['Bond_Type'].isin(["Agency","Government"])].index.tolist()
+basic_model.addConstr(gp.quicksum([basic_model.getVars()[x] for x in indexes_of_dec_var]) >= 4, \
                       "Government and Agency Bond"  )
 
+
 """third constraint Avg quality of Portfolio """
-basic_model.addConstr( (2*basic_model.getVars()[0] +
-                       2*basic_model.getVars()[1] +
-                       1*basic_model.getVars()[2] +
-                       1*basic_model.getVars()[3] + 
-                       5*basic_model.getVars()[4]) 
-                        <=1.4*(gp.quicksum(basic_model.getVars())),"Avg Quality of Portfolio")
+Quality_scale_bank = df['Quality_Scale_Bank'].tolist()
+basic_model.addConstr(gp.quicksum((a*b) for a,b in zip(Quality_scale_bank,basic_model.getVars()))\
+                      <=1.4*(gp.quicksum(basic_model.getVars())),"Avg Quality of Portfolio")
 
 """fourth constraint years of maturity"""
-basic_model.addConstr((9*basic_model.getVars()[0] +
-                       15*basic_model.getVars()[1] +
-                       4*basic_model.getVars()[2] +
-                       3*basic_model.getVars()[3] + 
-                       2*basic_model.getVars()[4]) 
-                        <=5*(gp.quicksum(basic_model.getVars())),"Years of maturity"  )
-
+Years_maturity=df['Years_Maturity'].tolist()
+basic_model.addConstr(gp.quicksum((a*b) for a,b in zip(Years_maturity,basic_model.getVars()))\
+                      <=5*(gp.quicksum(basic_model.getVars())),"Years of maturity")
 
 """fifth constraint equality constraints"""
 basic_model.addConstrs(x >=0  for x in basic_model.getVars())
 
 """writting the linear programming to understand what we have written is right or wrong """
-basic_model.write("prac_prob_week_1_advanced.lp")
+basic_model.write("Solving_Excel.lp")
 
 
 """optmizing the problem"""
 basic_model.optimize()
 
 """writting the solution file"""
-basic_model.write("prac_prob_week_1_advanced.sol")
+basic_model.write("Solving_Excel.sol")
 
 for v in basic_model.getVars():
     print('%s %g' % (v.VarName, v.X))
